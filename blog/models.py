e64fa6d905a0c777b008f32a8bc84929aea3a5f0
Eric Heitmann <85702659+h3its@users.noreply.github.com>
2022-09-26 14:21:49 -0400

    database = schemed

--- blog/models.py
+++ blog/models.py
@@ -6,9 +6,15 @@ class Author(models.Model):
     Name = models.CharField(max_length=30)
     Bio = models.TextField(max_length=120)
 
+    def __str__(self):
+        return self.Name
+
 class Topic(models.Model):
     Topic_Id = models.AutoField(primary_key=True)
     Name = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.Name
 
 class Post(models.Model):
     Post_Id = models.AutoField(primary_key=True)
@@ -18,3 +24,6 @@ class Post(models.Model):
     Date = models.DateField(auto_now_add=True)
     Author = models.ForeignKey(Author, on_delete=models.CASCADE)
     Topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.Title
